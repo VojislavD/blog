8064bed13e9dc04ba35584af640f2639180183a4
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-19 10:04:28 +0100

    Add Post model with migration and factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->realText(1500),
+            'published' => $this->faker->boolean()
+        ];
+    }
+}

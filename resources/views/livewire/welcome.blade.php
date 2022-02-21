277dd430b6994e7e5bd853b8adccd4fe0b51463d
VojislavD <vojislavdragicevic17@gmail.com>
2022-02-21 23:23:26 +0100

    Fix layout on welcome page

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -10,8 +10,8 @@
     </div>
     <div class="max-w-7xl mx-auto pb-16">
         <h1 class="text-center text-2xl font-bold">{{ __('Lastest Posts') }}</h1>
-        <div class="grid grid-cols-4 gap-8 mt-12">
-            @forelse ($posts as $post)
+        @forelse ($posts as $post)
+            <div class="grid grid-cols-4 gap-8 mt-12">
                 <div class="h-[400px] border border-gray-300 shadow-xl rounded-lg overflow-hidden">
                     <img class="w-full h-[180px] rounded-tl-lg rounded-tr-lg" src="{{ $post->displayFeaturedImage() }}">
                     <div class="h-[220px] flex flex-col justify-between px-4 py-3">
@@ -24,11 +24,11 @@
                         </div>
                     </div>
                 </div>
-            @empty
-                <p class="w-full text-center mt-8">{{ ('There is no posts yet.') }}</p>
-            @endforelse
-        </div>
-        @if(!empty($posts))
+            </div>
+        @empty
+            <p class="w-full text-center mt-12">{{ ('There is no posts yet.') }}</p>
+        @endforelse
+        @if($posts->count() > $amount)
             <div class="w-full flex items-center justify-center mt-16">
                 <button wire:click="loadMore" class="bg-blue-200 hover:bg-blue-300 px-4 py-2 text-blue-900 font-bold text-sm rounded-lg transition duration-200">{{ __('Load More...')}}</button>
             </div>

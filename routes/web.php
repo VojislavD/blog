64e73aec85959860358b08cf041a0346570c0738
vojislavd <vojislavdragicevic17@gmail.com>
2022-03-10 21:54:09 +0100

    Change default route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/admin');
-Route::redirect('/login', '/admin/login')->name('login');
-
 // Route::get('/', Welcome::class)->name('welcome');
 // Route::get('/posts/{post:slug}', Show::class)->name('posts.show');
